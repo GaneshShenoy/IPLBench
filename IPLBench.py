5eda08d8c083df86dd73a6d51c08d4965b38f096
GaneshShenoy <iganeshshenoy@hotmail.com>
2020-07-05 20:43:09 +0530

    Implement complete functionalities with print values

--- IPLBench.py
+++ IPLBench.py
@@ -5,7 +5,11 @@ class IPL:
         self.PlayerTeam = []
         self.edges = [[0] * max_num_vertexes for _ in range(max_num_vertexes)]    
         self.num_of_vertex = 0
-    
+        self.file_output = open("outputPS10.txt", "a")
+        
+    def __del__(self): 
+        self.file_output.close()
+        
     def add_vertex(self, vertex):                                   
         if vertex not in self.PlayerTeam: 
             self.PlayerTeam.append(vertex)
@@ -34,7 +38,6 @@ class IPL:
             input_file_reader.close()      
     
     def displayAll(self):
-        print ("--------Function displayFranchises --------")
         list_franchise= []
         list_players = []
         found_franchise = 0
@@ -51,16 +54,18 @@ class IPL:
             if (found_player):
                 list_franchise.append(self.PlayerTeam[i])
                 num_of_franchise += 1
-            
-            
+         
+        print ("--------Function displayAll--------")
         print ("Total no. of franchises: ", num_of_franchise)
         print ("Total no. of players: ", num_of_players)
         
-        print ("\nList of Franchises")
+        print ("\nList of Franchises:")
         print (*list_franchise, sep ="\n")
         
-        print ("\nList of players")
+        print ("\nList of players:")
         print (*list_players, sep ="\n")
+        print ("-----------------------------------")
+        
         
     def displayFranchises(self, player):    
         franchise_list = []
@@ -74,8 +79,11 @@ class IPL:
                     if self.PlayerTeam[i] not in franchise_list: 
                         franchise_list.append(self.PlayerTeam[i])
         
-        print("\n--------Function displayFranchises --------")
+        print("--------Function displayFranchises --------")
+        print("Player name: {0}".format(player))
+        print("List of Franchises:")
         print (*franchise_list, sep ="\n")
+        print("-------------------------------------------")
         
     def displayPlayers(self, franchise):
         players_list = []
@@ -89,9 +97,11 @@ class IPL:
                 if self.PlayerTeam[j] not in players_list: 
                     players_list.append(self.PlayerTeam[j])
         
-        print("\n--------Function displayPlayers --------")
+        print("--------Function displayPlayers --------")
+        print("Franchise name: {0}".format(franchise))
+        print("List of players:")
         print (*players_list, sep ="\n")
-        
+        print("----------------------------------------")
     
     def printMatrix(self, franchise):     
         print ("\n***********************")
@@ -101,6 +111,26 @@ class IPL:
             for j in range(self.num_of_vertex):
                 print(self.edges[i][j], end=' ')
             print()
+            
+    def getTeamIndexForPlayers(self, playerA, playerB):
+        player_a_index = self.get_vertex_index(playerA)
+        player_b_index = self.get_vertex_index(playerB)
+        
+        # considering player in maximum 2 teams
+        playerA_teams = [None]*2
+        playerB_teams = [None]*2
+        for i in range(self.num_of_vertex):
+            if (self.edges[i][player_a_index]):
+                if (playerA_teams[0] == None) :
+                    playerA_teams[0] = i
+                else :
+                    playerA_teams[1] = i
+            if (self.edges[i][player_b_index]):
+                if (playerB_teams[0] == None) :
+                    playerB_teams[0] = i
+                else :
+                    playerB_teams[1] = i
+        return playerA_teams, playerB_teams
         
     def franchiseBuddies(self, playerA, playerB):
         # Get the index of both the players
@@ -113,18 +143,43 @@ class IPL:
         for i in range(self.num_of_vertex):
             if (self.edges[i][player_a_index] and self.edges[i][player_b_index]):
                 team_buddy.append(self.PlayerTeam[i])
-                count += 1
-               
-        print("\n--------Function franchiseBuddies --------")
-        print ("Player A: ", playerA)
-        print ("Player B: ", playerB)
+                count += 1              
+        
+        print("--------Function franchiseBuddies --------")
+        print("Player A: {0}".format(playerA))
+        print("Player B: {0}".format(playerB))
         if (count):
             print ("Franchise Buddies: Yes, ", *team_buddy)
         else:
             print ("They never playered together")
-        
+        print("------------------------------------------")
+         
     def findPlayerConnect(self, playerA, playerB):
-        pass
+        # Find which team the player belongs to for both the players
+        # loop through both the team and find where both the values are 1
+        playerA_teams, playerB_teams = self.getTeamIndexForPlayers(playerA, playerB)      
+        found_player = False;
+        
+        print ("--------Function findPlayerConnect --------")
+        print ("Player A: {0}".format(playerA))
+        print ("Player B: {0}".format(playerB))
+        for player_a in range (2) :
+            if (playerA_teams[player_a] == None): 
+                continue
+            
+            for player_b in range(2) :
+                if (playerB_teams[player_b] == None): 
+                    continue
+                
+                for edge in range(self.num_of_vertex):
+                    if (self.edges[playerA_teams[player_a]][edge] and self.edges[playerB_teams[player_b]][edge]):                       
+                        print ('Related: Yes, {0} > {1} > {2} > {3} > {4}'.format(playerA, self.PlayerTeam[playerA_teams[player_a]], self.PlayerTeam[edge], self.PlayerTeam[playerB_teams[player_b]], playerB))
+                        found_player = True
+
+        if (False == found_player):
+            print ("No common player found between planyers {0} and {1}".format(playerA, playerB))
+            
+        print ("-------------------------------------------")
         
 if __name__ == "__main__":
     iplBench = IPL()
@@ -133,6 +188,6 @@ if __name__ == "__main__":
     iplBench.displayFranchises("Andrew Tye")
     iplBench.displayPlayers("KKR")
     iplBench.franchiseBuddies("Krunal Pandya", "Ishan Kishan" )
-    iplBench.findPlayerConnect(None, None)
+    iplBench.findPlayerConnect("Kedar Jadhav", "Ishan Kishan")
     print('ALL IPL bench functionalities are executed')
     
