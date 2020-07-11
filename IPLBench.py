506b8d1e4b7a5ed328b802b6def570ab66d5ce0c
GaneshShenoy <iganeshshenoy@hotmail.com>
2020-07-11 19:38:06 +0530

    Reduce complexity of findPlayerConnect functioin

--- IPLBench.py
+++ IPLBench.py
@@ -36,7 +36,7 @@ class IPL:
     '''
     This funciton intialize the member variables of the the IPL class 
     '''
-    def __init__(self, max_num_vertexes = 70):
+    def __init__(self, max_num_vertexes = 90):
         
         self.PlayerTeam =  []                                                     # Unique team name and the player names
         self.edges = [[0] * max_num_vertexes for _ in range(max_num_vertexes)]    # Matrix to store directed edges (association)
@@ -70,8 +70,8 @@ class IPL:
     def add_edge(self, from_key, to_key):       
         #self.add_vertex(from_key) # this keey is already added.
         self.add_vertex(to_key)
-        self.edges[self.get_vertex_index(from_key)][self.get_vertex_index(to_key)] = 1      
-   
+        self.edges[self.get_vertex_index(from_key)][self.get_vertex_index(to_key)] = 1
+
     '''
     Read the input file for the data of team and players
     By default the input file name is inputPS10.txt
@@ -125,10 +125,11 @@ class IPL:
         for i in range(self.num_of_vertex):
             found_player = 0
             for j in range(self.num_of_vertex):
-                if (self.edges[i][j] != 0):
-                    list_players.append(self.PlayerTeam[j])
+                if self.edges[i][j] != 0:
                     found_player = 1
-                    num_of_players += 1
+                    if self.PlayerTeam[j] not in list_players:
+                        list_players.append(self.PlayerTeam[j])
+                        num_of_players += 1
             if (found_player):
                 list_franchise.append(self.PlayerTeam[i])
                 num_of_franchise += 1
@@ -145,17 +146,19 @@ class IPL:
     '''
     Display all the franchises for the given player
     '''
-    def displayFranchises(self, player):    
+    def displayFranchises(self, player):
+        if player not in self.PlayerTeam:
+            self.file_output.write ("\nPlayer {0} not in input file".format(player))
+            return
         franchise_list = []
         
         # get the index for the player
         player_index = self.get_vertex_index(player)
         
         for i in range(self.num_of_vertex):
-            for j in range(self.num_of_vertex):
-                if (self.edges[i][j] != 0 and j == player_index):
-                    if self.PlayerTeam[i] not in franchise_list: 
-                        franchise_list.append(self.PlayerTeam[i])
+            if self.edges[i][player_index]:
+                if self.PlayerTeam[i] not in franchise_list:
+                    franchise_list.append(self.PlayerTeam[i])
                         
         self.file_output.write ("\n\n--------Function displayFranchises --------")
         self.file_output.write ("\nPlayer name: {0}".format(player))
@@ -170,16 +173,16 @@ class IPL:
         players_list = []
         
         # get the index for the player
-        player_index = self.get_vertex_index(franchise)
+        franchise_index = self.get_vertex_index(franchise)
         
         
         for j in range(self.num_of_vertex):
-            if (self.edges[player_index][j] != 0):
+            if (self.edges[franchise_index][j] != 0):
                 if self.PlayerTeam[j] not in players_list: 
                     players_list.append(self.PlayerTeam[j])
         
         self.file_output.write ("\n\n--------Function displayPlayers --------")
-        self.file_output.write ("\nPlayer name: {0}".format(franchise))
+        self.file_output.write ("\nFranchise name: {0}".format(franchise))
         self.file_output.write ("\nList of players:\n")
         self.file_output.write ('\n'.join(players_list))
         self.file_output.write ("\n-------------------------------------------")
@@ -221,6 +224,9 @@ class IPL:
     Check whether the players are in the same franchise
     '''
     def franchiseBuddies(self, playerA, playerB):
+        if playerA not in self.PlayerTeam or playerB not in self.PlayerTeam:
+            self.file_output.write ("\nPlayer(s) {0} {1} not in input file".format(playerA, playerB))
+            return
         # Get the index of both the players
         # traverse through the column see where both the players have value 1
         # get that index and that is the index where they are in the same team
@@ -247,6 +253,9 @@ class IPL:
     Check wether the players are connected based on other player who shares the two franchises.
     '''
     def findPlayerConnect(self, playerA, playerB):
+        if playerA not in self.PlayerTeam or playerB not in self.PlayerTeam:
+            self.file_output.write ("\nPlayer(s) {0} {1} not in input file".format(playerA, playerB))
+            return
         # Find which team the player belongs to for both the players
         # loop through both the team and find where both the values are 1
         playerA_teams, playerB_teams = self.getTeamIndexForPlayers(playerA, playerB)      
@@ -256,6 +265,7 @@ class IPL:
         self.file_output.write  ("\nPlayer A: {0}".format(playerA))
         self.file_output.write  ("\nPlayer B: {0}".format(playerB))
         
+        '''
         # Player might be in maximum of two teams
         for player_a in range (2) :
             if (playerA_teams[player_a] == None): 
@@ -272,7 +282,31 @@ class IPL:
                         self.file_output.write  ('\nRelated: Yes, {0} > {1} > {2} > {3} > {4}'.format(playerA, self.PlayerTeam[playerA_teams[player_a]], 
                                                                                                       self.PlayerTeam[edge], self.PlayerTeam[playerB_teams[player_b]], playerB))
                         found_player = True
-
+        '''
+        for edge in range (self.num_of_vertex):
+            
+            if (playerA_teams[0] != None and playerA_teams[0] != None ): 
+                if (self.edges[playerA_teams[0]][edge] and self.edges[playerB_teams[0]][edge]):
+                    self.file_output.write  ('\nRelated: Yes, {0} > {1} > {2} > {3} > {4}'.format(playerA, self.PlayerTeam[playerA_teams[0]], 
+                                                                                                      self.PlayerTeam[edge], self.PlayerTeam[playerB_teams[0]], playerB))
+                    found_player = True
+            if (playerA_teams[0] != None and playerA_teams[1] != None ): 
+                if (self.edges[playerA_teams[0]][edge] and self.edges[playerB_teams[1]][edge]):
+                    self.file_output.write  ('\nRelated: Yes, {0} > {1} > {2} > {3} > {4}'.format(playerA, self.PlayerTeam[playerA_teams[0]], 
+                                                                                                      self.PlayerTeam[edge], self.PlayerTeam[playerB_teams[1]], playerB))
+                    found_player = True                                                                                                            
+           
+            if (playerA_teams[1] != None and playerA_teams[0] != None ): 
+                if (self.edges[playerA_teams[1]][edge] and self.edges[playerB_teams[0]][edge]):
+                    self.file_output.write  ('\nRelated: Yes, {0} > {1} > {2} > {3} > {4}'.format(playerA, self.PlayerTeam[playerA_teams[1]], 
+                                                                                                      self.PlayerTeam[edge], self.PlayerTeam[playerB_teams[0]], playerB))   
+                    found_player = True                                                                                                      
+            if (playerA_teams[1] != None and playerA_teams[1] != None ): 
+                if (self.edges[playerA_teams[1]][edge] and self.edges[playerB_teams[1]][edge]):
+                    self.file_output.write  ('\nRelated: Yes, {0} > {1} > {2} > {3} > {4}'.format(playerA, self.PlayerTeam[playerA_teams[1]], 
+                                                                                                      self.PlayerTeam[edge], self.PlayerTeam[playerB_teams[1]], playerB)) 
+                    found_player = True                                                                                                      
+        
         if (False == found_player):
             self.file_output.write  ("\nNo common player found between planyers {0} and {1}".format(playerA, playerB))
             
